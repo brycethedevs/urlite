302d8a517a70a807fc67ca43031e2375810bee80
Bryce <46429473+brycethedeveloper@users.noreply.github.com>
2022-06-28 17:40:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,27 +5,7 @@ const adapter = new fs("db.json");
 const db = lowdb(adapter);
 const body = require("body-parser").json();
 const express = require("express");
-const helmet = require("helmet");
-
 const app = express();
-
-app.use(helmet());
-const helmat = require("helmet")
-app.use(helmet.contentSecurityPolicy());
-app.use(helmet.crossOriginEmbedderPolicy());
-app.use(helmet.crossOriginOpenerPolicy());
-app.use(helmet.crossOriginResourcePolicy());
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.expectCt());
-app.use(helmet.frameguard());
-app.use(helmet.hidePoweredBy());
-app.use(helmet.hsts());
-app.use(helmet.ieNoOpen());
-app.use(helmet.noSniff());
-app.use(helmet.originAgentCluster());
-app.use(helmet.permittedCrossDomainPolicies());
-app.use(helmet.referrerPolicy());
-app.use(helmet.xssFilter());
 
 
 app.use(express.static("public"));
