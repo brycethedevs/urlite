1b1d4eeddf81047766cbed417626b36d061e3ae8
Bryce <46429473+brycethedeveloper@users.noreply.github.com>
2022-06-28 18:04:42 +0000

    h

--- public/index.js
+++ public/index.js
@@ -14,9 +14,7 @@ form.addEventListener("submit", event => {
     .then(response => response.json())
     .then(json => {
       if (json.success === true) {
-        response.innerHTML = `<p><b>Your short url is: </b><a href="https://${window.location.hostname}/${json.slug}">https://${window.location.hostname}/${json.slug}</a></p>
-        <p><b>Your delete token is:</b></p>
-        <input class="copy" readonly="readonly" value="${json.token}" id="token" onclick="copytoken()" />
+        response.innerHTML = `<p><b>Created URL Is: </b><a href="https://${window.location.hostname}/${json.slug}">https://${window.location.hostname}/${json.slug}</a></p>
         `;
       } else {
         response.innerHTML = `<p><b>Error: </b>${json.error}</p>`;
