2de026f2097596fb6bb424b5f6b79398c08005b9
brycethedeveloper <46429473+brycethedeveloper@users.noreply.github.com>
2022-08-19 12:47:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -24,29 +24,37 @@ app.use(helmet.permittedCrossDomainPolicies());
 app.use(helmet.referrerPolicy());
 app.use(helmet.xssFilter());
 
+
 app.use(express.static("public"));
 
 app.set("json spaces", 2);
-
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/stats", (req, res) => {
+  res.sendFile(__dirname + "/views/stats.html");
+});
+
+
+app.get("/delete", (req, res) => {
+  res.sendFile(__dirname + "/views/delete.html");
+});
 
 
 app.post("/create", body, (req, res) => {
-
   var url = req.body.url;
   var slug = req.body.slug;
-
-
   if (!url)
     return res
       .status(400)
       .json({ success: false, error: "No URL was provided." });
 
-
+  if (checkurl(url) === false)
+    return res
+      .status(400)
+      .json({ success: false, error: "The URL provided is invalid" });
 
   if (url.includes(req.get("host")))
     return res.status(400).json({
@@ -54,17 +62,18 @@ app.post("/create", body, (req, res) => {
       error: "Long URLs cannot point to the URL shortener domain."
     });
 
-  
 
+  const token = random(30);
 
   if (slug) {
+
     if (!slug.match(/^[A-Za-z0-9_-]+$/))
       return res.status(400).json({
         success: false,
-        error: "The custom URL may only contain letters, numbers, dashes and underscores."
+        error: "Slug may only contain letters, numbers, dashes and underscores."
       });
 
-    
+
     if (
       db
         .get("urls")
@@ -78,19 +87,17 @@ app.post("/create", body, (req, res) => {
 
 
     db.get("urls")
-      .push({ slug: slug, url: url })
+      .push({ slug: slug, url: url, token: token, stats: 0 })
       .write();
     
 
     return res
       .status(200)
-      .json({ success: true, slug: slug, url: url });
-
+      .json({ success: true, slug: slug, url: url, token: token });
 
   } else {
 
     var slug = random(5);
-
 
     while (
       db
@@ -103,23 +110,75 @@ app.post("/create", body, (req, res) => {
 
 
     db.get("urls")
-      .push({ slug: slug, url: url })
+      .push({ slug: slug, url: url, token: token, stats: 0 })
       .write();
-
 
     return res
       .status(200)
-      .json({ success: true, slug: slug, url: url });
+      .json({ success: true, slug: slug, url: url, token: token });
   }
 });
 
+app.post("/stats", body, (req, res) => {
+  var slug = req.body.slug;
 
+  if (!slug)
+    return res.status(400).json({ success: false, error: "Slug is missing." });
+
+
+  const result = db
+    .get("urls")
+    .find({ slug: slug })
+    .value();
+
+
+  if (!result)
+    return res.status(400).json({ success: false, error: "Invalid slug." });
+
+  return res.status(200).json({
+    success: true,
+    slug: result.slug,
+    url: result.url,
+    stats: result.stats
+  });
+});
+
+
+app.post("/delete", body, (req, res) => {
+  var token = req.body.token;
+  var slug = req.body.slug;
+
+
+  if (!slug || !token)
+    return res
+      .status(400)
+      .json({ success: false, error: "Slug or token is missing." });
+
+
+  const result = db
+    .get("urls")
+    .find({ slug: slug, token: token })
+    .value();
+
+
+  if (!result)
+    return res
+      .status(400)
+      .json({ success: false, error: "Invalid slug or token." });
+
+
+  db.get("urls")
+    .remove({ slug: slug, token: token })
+    .write();
+
+
+  return res.status(200).json({ success: true });
+});
 
 
 app.get("*", (req, res) => {
 
   const slug = req.path.slice(1);
-
 
   const result = db
     .get("urls")
@@ -129,20 +188,20 @@ app.get("*", (req, res) => {
 
   if (!result) return res.status(404).sendFile(__dirname + "/views/404.html");
 
-  
+
   db.get("urls")
     .find({ slug: slug })
     .assign({ stats: result.stats + 1 })
     .write();
+
 
   return res.redirect(result.url);
 });
 
 
 const listener = app.listen(80, () => {
-  console.log();
+  console.log("Your app is listening on port " + listener.address().port);
 });
-
 
 function checkurl(string) {
   var url = "";
