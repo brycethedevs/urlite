99a4353610d588d7a3461fe3e947eeb66d818666
Bryce <46429473+brycethedeveloper@users.noreply.github.com>
2022-08-28 13:35:21 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -17,6 +17,7 @@ form.addEventListener("submit", event => {
         response.innerHTML = `<p><b>Created URL Is: </b><a href="https://${window.location.hostname}/${json.slug}">https://${window.location.hostname}/${json.slug}</a></p>
         <p><b>Your delete token is:</b></p>
         <input class="copy" readonly="readonly" value="${json.token}" id="token" onclick="copytoken()" />
+          <a href="./delete">Delete a slug</a>
         `;
       } else {
         response.innerHTML = `<p><b>Error: </b>${json.error}</p>`;
