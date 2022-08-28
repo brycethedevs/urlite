71595f1add02b72598af38a32846a06a31c22c42
Bryce <46429473+brycethedeveloper@users.noreply.github.com>
2022-08-28 13:26:30 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -15,6 +15,8 @@ form.addEventListener("submit", event => {
     .then(json => {
       if (json.success === true) {
         response.innerHTML = `<p><b>Created URL Is: </b><a href="https://${window.location.hostname}/${json.slug}">https://${window.location.hostname}/${json.slug}</a></p>
+        <p><b>Your delete token is:</b></p>
+        <input class="copy" readonly="readonly" value="${json.token}" id="token" onclick="copytoken()" />
         `;
       } else {
         response.innerHTML = `<p><b>Error: </b>${json.error}</p>`;
@@ -23,3 +25,9 @@ form.addEventListener("submit", event => {
     });
 });
 
+function copytoken() {
+  document.getElementById("token").select();
+  document.execCommand("copy");
+  const notyf = new Notyf();
+  notyf.success("Delete Token Copied to Clipboard");
+}
