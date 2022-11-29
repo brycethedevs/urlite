d49052c1a4b13497cac7350d5beabeecaf7dc17b
Bryce <46429473+brycethedeveloper@users.noreply.github.com>
2022-11-29 13:37:28 +0000

    big update

--- server.js
+++ server.js
@@ -5,15 +5,8 @@ const db = lowdb(adapter);
 const body = require("body-parser").json();
 const express = require("express");
 const app = express();
-const helmet = require("helmet")
-const auth = require('http-auth');
-const basic = auth.basic({realm: 'Monitor Area'}, function(user, pass, callback) {
-  callback(user === 'brycecarpenter' && pass === 'bRYCE2007@@>');
-});
-
-const statusMonitor = require('express-status-monitor')({ path: '/admin' });
-app.use(statusMonitor.middleware); 
-app.get('/status', basic.check(statusMonitor.pageRoute)); 
+const helmet = require("helmet");
+app.use(require('express-status-monitor')());
 
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy());
@@ -38,6 +31,7 @@ app.set("json spaces", 2);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
 
 app.post("/create", body, (req, res) => {
   let url = req.body.url;
@@ -108,6 +102,7 @@ app.post("/create", body, (req, res) => {
   }
 });
 
+
 app.get("*", (req, res) => {
 
   const slug = req.path.slice(1);
@@ -118,7 +113,143 @@ app.get("*", (req, res) => {
     .value();
 
 
-  if (!result) return res.status(404).sendFile(__dirname + "/views/404.html");
+ 
+    app.use((err, req, res, next) => {
+      res.send(`<!DOCTYPE html>
+      <html lang="en">
+        <head>
+        <meta charset="UTF-8" />
+         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+       <link rel="icon" type="image/png" href="./urlite.webp"/>
+       <meta name="title" content="Googlie ">
+      <meta name="description" content="URL shortener that fast and very simple just one click of a button and you will have your URL forever. What are you waiting for come shorten your URL now!     ">
+      <meta name="keywords" content="URL shortener,URL,shortener,fast URL shortener,ad free,Googlie,Nerver sell your data">
+      <meta name="robots" content="index, follow">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="language" content="English">
+      <style>
+      body, html {
+        height: 95%;
+      }
+      
+      body {
+        background: #1C1F29;
+        text-align: center;
+        font-family: "Nunito", sans-serif;
+        color: white;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+      
+      nav {
+        width: 100%;
+      }
+      
+      nav h1 {
+        color: #32DAF5;
+        font-weight: 900;
+        font-size: 3rem;
+        margin-left: 20px;
+      }
+      
+      #urltoshorten {
+        background: #2C333D;
+        color: white;
+        padding: 20px 30px;
+        box-shadow: 0px 9px 15px 0 rgba(0, 0, 0, 0.2);
+        border-radius: 10rem;
+        width: 65vw;
+        border: 1px solid transparent;
+        transition: .3s;
+      }
+      
+      form {
+        display: flex;
+        flex-direction: row;
+        text-align: center;
+        margin-top: 20px;
+        margin-bottom: 20px;
+      }
+      
+      #urltoshorten:focus {
+        border: 1px solid rgba(50, 218, 345, .5);
+      }
+      
+      input {
+        border: none;
+      }
+      
+      input:focus {
+        /* removing the input focus blue box. Put this on the form if you like. */
+        outline: none;
+      }
+      
+      button {
+        background: #32DAF5;
+        color: white;
+        border: none;
+        transform: scale(0.9);
+        margin-left: -55px;
+        cursor: pointer;
+        width: 4.5em;
+        border-radius: 10rem;
+        box-shadow: 0 1px 3px 0 #32DAF5, 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      }
+      
+      .center {
+        display: flex;
+        justify-content: center; /* center horizontally */
+        align-items: center; /* center vertically */
+      }
+      
+      #vanityflex {
+          display: flex;
+        flex-direction: row;
+        text-align: center;
+      }
+      
+      #vanity {
+        background: #2C333D;
+        color: white;
+        padding: 20px 30px;
+        box-shadow: 0px 9px 15px 0 rgba(0, 0, 0, 0.2);
+        margin-left:1vw;
+        height: 1em;
+        border-radius: 10rem;
+        border: 1px solid transparent;
+        transition: .3s;
+      }
+      
+      #buttons {
+        background: #2C333D;
+        color: white;
+        padding: 20px 30px;
+        box-shadow: 0px 9px 15px 0 rgba(0, 0, 0, 0.2);
+        border-radius: 10rem;
+        width: 12vw;
+        display: block;
+        border: 1px solid transparent;
+        transition: .3s;
+      }
+      
+      #vanity:focus {
+        border: 1px solid rgba(50, 218, 345, .5);
+      }
+      </style>
+          <title>Urlite | 404</title>
+        </head>
+        <body>
+            <h1>404</h1>
+            <h4>That URL does not exist.</h4>
+        </body>
+      </html>
+      `)
+    })
 
 
   db.get("urls")
@@ -131,6 +262,7 @@ app.get("*", (req, res) => {
 });
 
 app.listen(80)
+
 
 function checkurl(string) {
   let url = "";
@@ -147,7 +279,8 @@ function random(length) {
   let result = "";
   const characters = "abcdefghijkmnopqrstuvwxyz0123456789";
   for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(math.getRandomValues() * characters.length));
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+
   }
   return result;
 }
